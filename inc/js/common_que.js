2fe60cdf819e6e7f655c109670f8c5aa79e7a276
yangz <yangz@chunjae.co.kr>
2022-12-22 09:13:37 +0900

    common js 수정

--- inc/js/common_que.js
+++ inc/js/common_que.js
@@ -9,98 +9,104 @@ $(function () {
       case 'sub01_03_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(0).find('a').addClass('on');
         break;
       case 'sub01_04_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(0).find('a').addClass('on');
         break;
       case 'sub02_01_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(1).find('a').addClass('on');
         break;
       case 'sub02_02_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(1).find('a').addClass('on');
         break;
       case 'sub02_03_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(1).find('a').addClass('on');
         break;
       case 'sub03_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(2).find('a').addClass('on');
         break; 
       case 'sub03_02':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(2).find('a').addClass('on');
         break;
       case 'sub03_03':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(2).find('a').addClass('on');
         break;
       case 'sub03_04':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(2).find('a').addClass('on');
         break;
       case 'page04_01':
         _gnbMenuLink.eq(0).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(0).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(0).find('.depth2').addClass('on');
         _gnbMenuLink.eq(0).find('.depth2').find('li').eq(3).find('a').addClass('on');
         break;      
       case 'page06_01':
         _gnbMenuLink.eq(1).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(1).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(1).find('.depth2').addClass('on');
         _gnbMenuLink.eq(1).find('.depth2').find('li').eq(0).find('a').addClass('on');
         break;
       case 'page06_02':
         _gnbMenuLink.eq(1).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(1).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(1).find('.depth2').addClass('on');
         _gnbMenuLink.eq(1).find('.depth2').find('li').eq(1).find('a').addClass('on');
         break;
       case 'page06_03':
         _gnbMenuLink.eq(1).find('> a').addClass('active');
         $('.nav-wrap').addClass('on');
-        _gnbMenuLink.eq(1).find('.depth2').css('display', 'flex');
+        _gnbMenuLink.eq(1).find('.depth2').addClass('on');
         _gnbMenuLink.eq(1).find('.depth2').find('li').eq(2).find('a').addClass('on');
         break;
       case 'page06_04':
       _gnbMenuLink.eq(1).find('> a').addClass('active');
       $('.nav-wrap').addClass('on');
-      _gnbMenuLink.eq(1).find('.depth2').css('display', 'flex');
+      _gnbMenuLink.eq(1).find('.depth2').addClass('on');
       _gnbMenuLink.eq(1).find('.depth2').find('li').eq(3).find('a').addClass('on');
         break;
       case 'page10_01':
       _gnbMenuLink.eq(2).find('> a').addClass('active');
       $('.nav-wrap').addClass('on');
-      _gnbMenuLink.eq(2).find('.depth2').css('display', 'flex');
+      _gnbMenuLink.eq(2).find('.depth2').addClass('on');
       _gnbMenuLink.eq(2).find('.depth2').find('li').eq(0).find('a').addClass('on');
         break;
       case 'page12_01':
       _gnbMenuLink.eq(2).find('> a').addClass('active');
       $('.nav-wrap').addClass('on');
-      _gnbMenuLink.eq(2).find('.depth2').css('display', 'flex');
+      _gnbMenuLink.eq(2).find('.depth2').addClass('on');
       _gnbMenuLink.eq(2).find('.depth2').find('li').eq(2).find('a').addClass('on');
+        break;
+      case 'page13_01':
+      _gnbMenuLink.eq(3).find('> a').addClass('active');
+      $('.nav-wrap').addClass('on');
+      _gnbMenuLink.eq(3).find('.depth2').addClass('on');
+      _gnbMenuLink.eq(3).find('.depth2').find('li').eq(0).find('a').addClass('on');
         break;
     }
   }
