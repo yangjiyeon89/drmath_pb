fec6358c5f131664efdc128c7ea5f387027056bd
yangz <yangz@chunjae.co.kr>
2023-02-07 13:13:11 +0900

    header  수정

--- inc/js/admin.js
+++ inc/js/admin.js
@@ -22,30 +22,35 @@ $(function () {
     }
     switch (_afterStr[0]) {
       case 'admin08':
-        _gnbMenuLink.eq(1).addClass('on');
-        _gnbMenuLink.eq(1).find('.depth2').find('li').eq(0).find('a').addClass('on');
-        _allMenuLink.find('ul').eq(1).find('li').eq(1).addClass('on')
-        break;
-    }
-    switch (_afterStr[0]) {
-      case 'admin09':
         _gnbMenuLink.eq(2).addClass('on');
         _gnbMenuLink.eq(2).find('.depth2').find('li').eq(0).find('a').addClass('on');
         _allMenuLink.find('ul').eq(2).find('li').eq(1).addClass('on')
         break;
     }
     switch (_afterStr[0]) {
+      case 'admin09':
+        _gnbMenuLink.eq(3).addClass('on');
+        _gnbMenuLink.eq(3).find('.depth2').find('li').eq(0).find('a').addClass('on');
+        _allMenuLink.find('ul').eq(3).find('li').eq(1).addClass('on')
+        break;
+    }
+    switch (_afterStr[0]) {
       case 'admin10':
-        _gnbMenuLink.eq(2).addClass('on');
-        _gnbMenuLink.eq(2).find('.depth2').find('li').eq(1).find('a').addClass('on');
-        _allMenuLink.find('ul').eq(2).find('li').eq(2).addClass('on')
+        _gnbMenuLink.eq(3).addClass('on');
+        _gnbMenuLink.eq(3).find('.depth2').find('li').eq(1).find('a').addClass('on');
+        _allMenuLink.find('ul').eq(3).find('li').eq(2).addClass('on')
         break;
     }
     switch (_afterStr[0]) {
       case 'admin11':
-        _gnbMenuLink.eq(2).addClass('on');
-        _gnbMenuLink.eq(2).find('.depth2').find('li').eq(2).find('a').addClass('on');
-        _allMenuLink.find('ul').eq(2).find('li').eq(3).addClass('on')
+        _gnbMenuLink.eq(3).addClass('on');
+        _gnbMenuLink.eq(3).find('.depth2').find('li').eq(2).find('a').addClass('on');
+        _allMenuLink.find('ul').eq(3).find('li').eq(3).addClass('on')
+        break;
+    }
+    switch (_afterStr[0]) {
+      case 'admin12':
+        _gnbMenuLink.eq(0).addClass('on');
         break;
     }
 
